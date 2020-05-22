0a58ffa4b5c28ff9eb85a16310564a089079ba3f
kuldeep.gaur <kuldeep.gaur@franconnect.com>
2020-05-22 17:41:55 +0530

    Initial Commit

---
+++ leetCode/easy/HowManyNumberLessThenCurrent.java
@@ -0,0 +1,15 @@
+package leetCode.easy;
+
+public class HowManyNumberLessThenCurrent {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+	public static int[] getNumberCountLessThenCurrentNumber(int [] num){
+		for(int number: num){
+
+		}
+
+		return num;
+	}
+}
